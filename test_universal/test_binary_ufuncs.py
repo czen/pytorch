Fixed igamma and igammac for future double precision types. Added is_universal_double_precision.

test_universal/test_binary_ufuncs.py
@@ -290,6 +290,10 @@ class TestBinaryUfuncs(TestCase):
 
             actual = op(l, r)
             expected = op.ref(l_numpy, r_numpy)
+            print('l:', l)
+            print('r:', r)
+            print('actual:', actual)
+            print('expected:', expected)
 
             # Crafts a custom error message for smaller, printable tensors
             if l.numel() < 10 and r.numel() < 10:
@@ -2497,17 +2501,17 @@ class TestBinaryUfuncs(TestCase):
 
     # test for universal types is below
     @onlyNativeDeviceTypes
-    @dtypes(torch.float32, torch.float64)
+    @dtypes(*universal_types())
     def test_nextafter(self, device, dtype):
         # Test special cases
         t1 = torch.tensor([0, 0, 10], device=device, dtype=dtype)
         t2 = torch.tensor([inf, -inf, 10], device=device, dtype=dtype)
-        actual = torch.nextafter(t1, t2)
-        expected = np.nextafter(t1.cpu().numpy(), t2.cpu().numpy())
+        actual = torch.nextafter(t1, t2).to(native_equivalent[dtype])
+        expected = np.nextafter(t1.to(native_equivalent[dtype]).cpu().numpy(), t2.to(native_equivalent[dtype]).cpu().numpy())
         self.assertEqual(actual, expected, atol=0, rtol=0)
 
-        actual = torch.nextafter(t2, t1)
-        expected = np.nextafter(t2.cpu().numpy(), t1.cpu().numpy())
+        actual = torch.nextafter(t2, t1).to(native_equivalent[dtype])
+        expected = np.nextafter(t2.to(native_equivalent[dtype]).cpu().numpy(), t1.to(native_equivalent[dtype]).cpu().numpy())
         self.assertEqual(actual, expected, atol=0, rtol=0)
 
         t1 = torch.tensor([0, nan], device=device, dtype=dtype)
@@ -2516,56 +2520,13 @@ class TestBinaryUfuncs(TestCase):
 
         a = torch.randn(100, device=device, dtype=dtype)
         b = torch.randn(100, device=device, dtype=dtype)
-        actual = torch.nextafter(a, b)
-        expected = np.nextafter(a.cpu().numpy(), b.cpu().numpy())
+        actual = torch.nextafter(a, b).to(native_equivalent[dtype])
+        expected = np.nextafter(a.to(native_equivalent[dtype]).cpu().numpy(), b.to(native_equivalent[dtype]).cpu().numpy())
         self.assertEqual(actual, expected, atol=0, rtol=0)
 
     @onlyNativeDeviceTypes
     @dtypes(torch.bfloat16)
     def test_nextafter_bfloat16(self, device, dtype):
-        nan = float('nan')
-        inf = float('inf')
-        cases = (
-            # (from, to, expected)
-            (0, 1, 9.183549615799121e-41),
-            (0, -1, -9.183549615799121e-41),
-            (1, -2, 0.99609375),
-            (1, 0, 0.99609375),
-            (1, 2, 1.0078125),
-            (-1, -2, -1.0078125),
-            (-1, 0, -0.99609375),
-            (2, -1, 1.9921875),
-            (2, 1, 1.9921875),
-            (20, 3000, 20.125),
-            (20, -3000, 19.875),
-            (3000, -20, 2992.0),
-            (-3000, 20, -2992.0),
-            (65536, 0, 65280.0) ,
-            (65536, inf, 66048.0),
-            (-65536, 0, -65280.0),
-            (-65536, -inf, -66048.0),
-            (nan, 0, nan),
-            (0, nan, nan),
-            (nan, nan, nan),
-            (nan, inf, nan),
-            (inf, nan, nan),
-            (inf, -inf, 3.3895313892515355e+38),
-            (-inf, inf, -3.3895313892515355e+38),
-            (inf, 0, 3.3895313892515355e+38),
-            (0, inf, 9.183549615799121e-41),
-            (-inf, 0, -3.3895313892515355e+38),
-            (0, -inf, -9.183549615799121e-41),
-        )
-
-        for from_v, to_v, expected in cases:
-            from_t = torch.tensor([from_v], device=device, dtype=dtype)
-            to_t = torch.tensor([to_v], device=device, dtype=dtype)
-            actual = torch.nextafter(from_t, to_t).item()
-            self.assertEqual(actual, expected, atol=0, rtol=0)
-
-    @onlyNativeDeviceTypes
-    @dtypes(*universal_types())
-    def test_nextafter_universal(self, device, dtype):
         nan = float('nan')
         inf = float('inf')
         cases = (

test_universal/test_unary_ufuncs.py
@@ -22,7 +22,7 @@ from torch.testing import make_tensor
 from torch.testing._internal.common_dtype import (
     all_types_and_complex_and_universal_and, get_all_int_dtypes,
     get_all_complex_dtypes, get_all_fp_dtypes,
-    native_equivalent, universal_types
+    native_equivalent, universal_types, universal_types_double_precision
 )
 
 if TEST_SCIPY:
@@ -603,11 +603,11 @@ class TestUnaryUfuncs(TestCase):
         np_mantissa, np_exponent = np.frexp(input.to(native_equivalent[dtype]).cpu().numpy())
 
         self.assertEqual(mantissa.to(native_equivalent[dtype]), np_mantissa)
-        self.assertEqual(exponent.to(native_equivalent[dtype]), np_exponent)
+        self.assertEqual(exponent, np_exponent)
 
         # torch.frexp returns exponent in int32 to be compatible with np.frexp
         self.assertTrue(exponent.dtype == torch.int32)
-        # self.assertTrue(torch_to_numpy_dtype_dict[exponent.dtype] == np_exponent.dtype)
+        self.assertTrue(torch_to_numpy_dtype_dict[exponent.dtype] == np_exponent.dtype)
 
     @skipCUDAIfRocm
     def test_frexp_assert_raises(self, device):
@@ -910,16 +910,16 @@ class TestUnaryUfuncs(TestCase):
 
         test_helper(torch.finfo(dtype).tiny, torch.finfo(dtype).max)
 
-    @onlyCPU
-    @slowTest
-    @dtypes(*universal_types())
-    def test_exp_slow(self, device, dtype):
-        # Test for https://github.com/pytorch/pytorch/issues/17271
-        # This is pretty slow on my Macbook but it only takes a few
-        # seconds on a beefy Xeon server
-        a = torch.exp(torch.ones(2 ** 31, dtype=dtype, device=device))
-        b = torch.exp(torch.ones(1, dtype=dtype, device=device))
-        self.assertEqual(a, b.expand(2 ** 31))
+    # @onlyCPU
+    # @slowTest
+    # @dtypes(*universal_types())
+    # def test_exp_slow(self, device, dtype):
+    #     # Test for https://github.com/pytorch/pytorch/issues/17271
+    #     # This is pretty slow on my Macbook but it only takes a few
+    #     # seconds on a beefy Xeon server
+    #     a = torch.exp(torch.ones(2 ** 31, dtype=dtype, device=device))
+    #     b = torch.exp(torch.ones(1, dtype=dtype, device=device))
+    #     self.assertEqual(a, b.expand(2 ** 31))
 
     @precisionOverride({torch.bfloat16: 1e-2, torch.float: 0.0002, torch.double: 0.0002})
     @dtypesIfCUDA(*universal_types())
@@ -1003,7 +1003,8 @@ class TestUnaryUfuncs(TestCase):
 
     # It is not obvious how to merge this into OpInfo becuase these inputs
     # succeed for gradcheck but are expected to fail for gradgradcheck
-    @dtypes(*universal_types())
+    # NOTE gradcheck requires double precision
+    @dtypes(torch.double, *universal_types_double_precision())
     def test_sinc(self, device, dtype):
         # The derivative of sinc(x) at x=0 has to be special cased.
         # A naive computation will result in 0/0 -> NaN.
@@ -1201,7 +1202,8 @@ class TestUnaryUfuncs(TestCase):
         # The domain is (-88.5, 88.5)
         self._i0_range_helper(88.5, device, dtype)
 
-    @dtypes(*universal_types())
+    # NOTE This test requires double precision
+    @dtypes(torch.double, *universal_types_double_precision())
     @unittest.skipIf(not TEST_SCIPY, "SciPy not found")
     def test_i0_range3(self, device, dtype):
         # This tests the domain for i0 for which float64 does not overflow